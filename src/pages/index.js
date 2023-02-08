fb6501e96a67bb20736b36c3a253dcc101c6de88
ThiagoSartorel <thiagosartorel@uniplac.net>
2023-02-08 10:15:08 -0300

    formulario

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,22 @@ import Back from "../components/Back";
 import Tags from "../components/Tags";
 
 export default function Home() {
-  const [correctCode, setCorrectCode] = useState("11111")
+  const [correctCode, setCorrectCode] = useState("11111");
+  const [allowed, setAllowed] = useState(false);
+
+  function sendCode(){
+    document.getElementById("infoSendEmail").hidden = false;
+  }
 
   function validateCodde(code) {
     if (code.length == 5 && code == correctCode) {
       document.getElementById("codeOk").hidden = false;
       document.getElementById("codeWrong").hidden = true;
+      setAllowed(true);
     } else {
       document.getElementById("codeWrong").hidden = false;
       document.getElementById("codeOk").hidden = true;
+      setAllowed(false);
     }
     return;
   }
@@ -68,12 +75,13 @@ export default function Home() {
                         type="email"
                         id="email"
                       />
-                      <div className="flex items-center">
+                      <div className="flex items-center" onClick={(e) => sendCode()}>
                         <label className="text-center bg-blue-400 hover:bg-blue-500 px-2 rounded-r-lg h-full text-white flex items-center cursor-pointer">
                           Validar email
                         </label>
                       </div>
                     </div>
+                    <span id="infoSendEmail" hidden className="text-sm text-gray-500">Foi enviado um código para validação no seu e-mail.</span>
                   </div>
                   <div className="flex flex-row-reverse">
                     <label className="sr-only" htmlFor="code">
@@ -110,119 +118,134 @@ export default function Home() {
                     </div>
                   </div>
 
-                  <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-3">
-                    <div>
-                      <input
-                        className="sr-only"
-                        id="option1"
-                        type="radio"
-                        tabIndex="-1"
-                      />
-                      <label
-                        htmlFor="option1"
-                        className="block w-full rounded-lg border border-gray-200 p-3"
-                        tabIndex="0"
-                      >
-                        <span className="text-sm font-medium"> Option 1 </span>
-                      </label>
-                    </div>
+                  {allowed == true ? (
+                    <section>
+                      <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-3">
+                        <div>
+                          <input
+                            className="sr-only"
+                            id="option1"
+                            type="radio"
+                            tabIndex="-1"
+                          />
+                          <label
+                            htmlFor="option1"
+                            className="block w-full rounded-lg border border-gray-200 p-3"
+                            tabIndex="0"
+                          >
+                            <span className="text-sm font-medium">
+                              {" "}
+                              Option 1{" "}
+                            </span>
+                          </label>
+                        </div>
 
-                    <div>
-                      <input
-                        className="sr-only"
-                        id="option2"
-                        type="radio"
-                        tabIndex="-1"
-                      />
-                      <label
-                        htmlFor="option2"
-                        className="block w-full rounded-lg border border-gray-200 p-3"
-                        tabIndex="0"
-                      >
-                        <span className="text-sm font-medium"> Option 2 </span>
-                      </label>
-                    </div>
+                        <div>
+                          <input
+                            className="sr-only"
+                            id="option2"
+                            type="radio"
+                            tabIndex="-1"
+                          />
+                          <label
+                            htmlFor="option2"
+                            className="block w-full rounded-lg border border-gray-200 p-3"
+                            tabIndex="0"
+                          >
+                            <span className="text-sm font-medium">
+                              {" "}
+                              Option 2{" "}
+                            </span>
+                          </label>
+                        </div>
 
-                    <div>
-                      <input
-                        className="sr-only"
-                        id="option3"
-                        type="radio"
-                        tabIndex="-1"
-                      />
-                      <label
-                        htmlFor="option3"
-                        className="block w-full rounded-lg border border-gray-200 p-3"
-                        tabIndex="0"
-                      >
-                        <span className="text-sm font-medium"> Option 3 </span>
-                      </label>
-                    </div>
-                  </div>
+                        <div>
+                          <input
+                            className="sr-only"
+                            id="option3"
+                            type="radio"
+                            tabIndex="-1"
+                          />
+                          <label
+                            htmlFor="option3"
+                            className="block w-full rounded-lg border border-gray-200 p-3"
+                            tabIndex="0"
+                          >
+                            <span className="text-sm font-medium">
+                              {" "}
+                              Option 3{" "}
+                            </span>
+                          </label>
+                        </div>
+                      </div>
 
-                  <div>
-                    <label
-                      className="mb-2 text-sm text-gray-700 flex items-center"
-                      htmlFor="file_input"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 384 512"
-                        className="w-3 mr-2"
-                      >
-                        <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
-                      </svg>
-                      <p>Arquivo</p>
-                    </label>
-                    <input
-                      className="block w-full text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 focus:outline-none"
-                      aria-describedby="file_input_help"
-                      id="file_input"
-                      type="file"
-                    />
-                    <p
-                      className="mt-1 text-sm text-gray-500"
-                      id="file_input_help"
-                    >
-                      Apenas arquivos PDF.
-                    </p>
-                  </div>
-
-                  <div>
-                    <label className="sr-only" htmlFor="message">
-                      Message
-                    </label>
-                    <textarea
-                      className="w-full rounded-lg border-gray-200 p-3 text-sm"
-                      placeholder="Mensagem"
-                      rows="8"
-                      id="message"
-                    ></textarea>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="submit"
-                      className="inline-flex w-full items-center justify-center rounded-lg bg-black px-5 py-3 text-white sm:w-auto"
-                    >
-                      <span className="font-medium"> Enviar </span>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="ml-3 h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M14 5l7 7m0 0l-7 7m7-7H3"
+                      <div>
+                        <label
+                          className="mb-2 text-sm text-gray-700 flex items-center"
+                          htmlFor="file_input"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 384 512"
+                            className="w-3 mr-2"
+                          >
+                            <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
+                          </svg>
+                          <p>Arquivo</p>
+                        </label>
+                        <input
+                          className="block w-full text-sm text-gray-900 border border-gray-300 cursor-pointer bg-gray-50 focus:outline-none"
+                          aria-describedby="file_input_help"
+                          id="file_input"
+                          type="file"
                         />
-                      </svg>
-                    </button>
-                  </div>
+                        <p
+                          className="mt-1 text-sm text-gray-500"
+                          id="file_input_help"
+                        >
+                          Apenas arquivos PDF.
+                        </p>
+                      </div>
+
+                      <div>
+                        <label className="sr-only" htmlFor="message">
+                          Message
+                        </label>
+                        <textarea
+                          className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                          placeholder="Mensagem"
+                          rows="8"
+                          id="message"
+                        ></textarea>
+                      </div>
+
+                      <div className="mt-4">
+                        <button
+                          type="submit"
+                          className="inline-flex w-full items-center justify-center rounded-lg bg-black px-5 py-3 text-white sm:w-auto"
+                        >
+                          <span className="font-medium"> Enviar </span>
+
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="ml-3 h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M14 5l7 7m0 0l-7 7m7-7H3"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </section>
+                  ) : (
+                    ""
+                  )}
                 </form>
               </div>
             </div>
