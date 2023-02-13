26e9c36506c39dfda04d10cc13208375a04846a1
Uniplac <github@uniplaclages.edu.br>
2023-02-13 11:56:28 -0300

    back

--- src/pages/documento/[id].js
+++ src/pages/documento/[id].js
@@ -30,7 +30,9 @@ export default function Detail(props) {
 
   const [date, setDate] = useState(publi.date);
 
-  const [fileLink, setFileLink] = useState("http://172.16.248.88:1465/showFile/1004");
+  const [fileLink, setFileLink] = useState(
+    "http://172.16.248.88:1465/showFile/1004"
+  );
 
   return (
     <section className="bg-gray-100 tracking-normal">
@@ -39,7 +41,6 @@ export default function Detail(props) {
         <Tags />
         <div className="w-full lg:w-4/5 p-8 mt-6 lg:mt-0 text-gray-900 leading-normal bg-white border border-gray-400 border-rounded">
           <div className="font-sans">
-            <Back />
             <div className="flex flex-row-reverse gap-2">
               {tagsId.map((e, index) => (
                 <div key={index}>
