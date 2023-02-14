7cd01edb4f1e958591fd1b4377e317af61445561
ThiagoSartorel <thiagosartorel@uniplac.net>
2023-02-14 15:35:37 -0300

    form final

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 async function sendEmail(email, title, content) {
   console.log("chamou a função");
-  console.log(content)
   try {
-    //const send = await axios.post(process.env.API_EMAIL, {
-    //  destino: email,
-    //  assunto: title,
-    //  conteudo: content,
-    //});
+    const send = await axios.post(process.env.BACKEND + "sendEmail", {
+      destino: email,
+      assunto: title,
+      conteudo: "Seu código de ativação para permição de envio de documente é" + content + "",
+    });
+    console.log("enviou")
   } catch (e) {
     console.log(e);
   }
