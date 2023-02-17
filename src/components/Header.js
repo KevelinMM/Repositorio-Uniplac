0b8cad0b4a0bc55abb713d191eff59787bed562e
ThiagoSartorel <thiagosartorel@uniplac.net>
2023-02-17 09:51:15 -0300

    new search function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import { useState } from "react";
-import db from "../db/db"
+import db from "../db/db";
 
-const base = db.documents
-
+const base = db.documents;
 
 export default function Header() {
   const [result, setResult] = useState(base);
@@ -21,7 +20,6 @@ export default function Header() {
               alt="Logo Uniplac"
               width={60}
               height={60}
-
             />
             <a
               className="text-normal ml-2 text-gray-900 no-underline hover:no-underline font-bold md:text-xl"
@@ -64,46 +62,45 @@ export default function Header() {
                     <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
                   </svg>
                 </div>
-                <input
-                  type="search"
-                  id="search"
-                  placeholder="Pesquise por autor ou titulo do documento"
-                  className=" w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-blue-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"
-                  onChange={(e) => {
-                    setResult(
-                      base.filter(
-                        (element) =>
-                        e.target.value.length > 0 ?
-                          element.title
-                            .toLowerCase()
-                            .includes(e.target.value.toLowerCase()) ||
-                          element.subtitle
-                            .toLowerCase()
-                            .includes(e.target.value.toLowerCase()) ||
-                          element.autor
-                            .toLowerCase()
-                            .includes(e.target.value.toLowerCase())
-                            : ''
-                      )
-                    )
-                    setValueSearch(e.target.value);
-                    e.target.value != "" ? document.getElementById("result").hidden = false : document.getElementById("result").hidden = true
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    window.location.href = "/filtro/search-" + valueSearch
                   }}
-                />
+                >
+                  <input
+                    type="search"
+                    id="search"
+                    placeholder="Pesquise por autor ou titulo do documento"
+                    className=" w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-blue-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"
+                    onChange={(e) => setValueSearch(e.target.value)}
+                  />
+                </form>
                 <div className="flex w-full absolute cursor-pointer ">
-                  <div className="bg-gray-100 shadow border-l border-r border-b border-gray-500 rounded-b w-full mr-4" hidden id="result">
+                  <div
+                    className="bg-gray-100 shadow border-l border-r border-b border-gray-500 rounded-b w-full mr-4"
+                    hidden
+                    id="result"
+                  >
                     {result.map((e) => {
                       return (
-                        <div key={e.title} className="hover:bg-gray-200 px-2 border-b">
+                        <div
+                          key={e.title}
+                          className="hover:bg-gray-200 px-2 border-b"
+                        >
                           <p className="text-black text-base">{e.title}</p>
 
-                          <p className="text-black font-thin text-xs text-end">{e.autor}</p>
+                          <p className="text-black font-thin text-xs text-end">
+                            {e.autor}
+                          </p>
                         </div>
                       );
                     })}
                     <div className="hover:bg-gray-200 px-2 w-full">
-                      <a href={"/filtro/search-" + valueSearch} >
-                        {result.length >= 1 ? "Ver todos..." : "Nenhum resultado"}
+                      <a href={"/filtro/search-" + valueSearch}>
+                        {result.length >= 1
+                          ? "Ver todos..."
+                          : "Nenhum resultado"}
                       </a>
                     </div>
                   </div>
